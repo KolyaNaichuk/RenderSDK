3438d5decb1b1db67c8a09eb6369670ab9546adb
Kolya Naichuk <thyvanitas@gmail.com>
2015-11-28 17:25:06 +0100

    Creating bounding box from point list

--- Include/Math/BoundingBox.h
+++ Include/Math/BoundingBox.h
@@ -4,9 +4,10 @@
 
 struct BoundingBox
 {
-    BoundingBox(const Vector3f& center, const Vector3f& radius);
+	BoundingBox(const Vector3f& center, const Vector3f& radius);
+	BoundingBox(u32 numPoints, const Vector3f* pFirstPoint);
     BoundingBox(const BoundingBox& box1, const BoundingBox& box2);
-	    
+		    
     Vector3f m_Center;
     Vector3f m_Radius;
 };
--- Source/Math/BoundingBox.cpp
+++ Source/Math/BoundingBox.cpp
@@ -7,6 +7,23 @@ BoundingBox::BoundingBox(const Vector3f& center, const Vector3f& radius)
 {
 }
 
+BoundingBox::BoundingBox(u32 numPoints, const Vector3f* pFirstPoint)
+{
+	Vector3f minPoint = *pFirstPoint;
+	Vector3f maxPoint = *pFirstPoint;
+
+	for (u32 pointIndex = 1; pointIndex < numPoints; ++pointIndex)
+	{
+		const Vector3f& point = *(pFirstPoint + pointIndex);
+
+		minPoint = Min(minPoint, point);
+		maxPoint = Max(maxPoint, point);
+	}
+
+	m_Center = 0.5f * (minPoint + maxPoint);
+	m_Radius = maxPoint - m_Center;
+}
+
 BoundingBox::BoundingBox(const BoundingBox& box1, const BoundingBox& box2)
 {
     Vector3f minPoint = Min(box1.m_Center - box1.m_Radius, box2.m_Center - box2.m_Radius);
