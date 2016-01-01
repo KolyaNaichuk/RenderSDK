a0a5a50ef4474fd592a180c6ae8f976d5960e247
Kolya Naichuk <thyvanitas@gmail.com>
2016-01-01 18:38:56 +0100

    Enabled ReportLiveObjects functionality

--- Include/Common/Common.h
+++ Include/Common/Common.h
@@ -5,6 +5,8 @@
 #include <windows.h>
 
 #include <d3d12.h>
+#include <dxgidebug.h>
+#include <dxgi1_3.h>
 #include <dxgi1_4.h>
 #include <D3Dcompiler.h>
 #include <wrl.h>
--- Source/DX/DXDebug.cpp
+++ Source/DX/DXDebug.cpp
@@ -8,4 +8,8 @@ DXDebug::DXDebug()
 void DXDebug::EnableDebugLayer()
 {
 	GetDXObject()->EnableDebugLayer();
+	
+	ComPtr<IDXGIDebug1> dxgiDebug;
+	DXVerify(DXGIGetDebugInterface1(0, IID_PPV_ARGS(&dxgiDebug)));
+	dxgiDebug->ReportLiveObjects(DXGI_DEBUG_ALL, DXGI_DEBUG_RLO_ALL);
 }
