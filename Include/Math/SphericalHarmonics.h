a9c0caaae3108019806b489f3605375ac76a7225
Kolya Naichuk <thyvanitas@gmail.com>
2016-02-26 19:14:38 +0100

    More functionality for Spherical Harmonics

--- Include/Math/SphericalHarmonics.h
+++ Include/Math/SphericalHarmonics.h
@@ -9,5 +9,8 @@ f32 SHEvaluate(i32 l, i32 m, const Radian& theta, const Radian& phi);
 
 i32 SHGetBasisFuncIndex(i32 l, i32 m);
 i32 SHGetNumBasisFuncs(i32 numBands);
-void SHEvaluateBasis(f32* pBasisFuncValues, i32 numBands, const Vector3f& dir);
-void ZHProjectClampedCosineFunc(f32* pCoeffs, i32 numBands);
+void SHEvaluateBasis(f32* pOutBasisFuncValues, i32 numBands, const Vector3f& dir);
+void SHAdd(f32* pOutCoeffs, const f32* pCoeffs1, const f32* pCoeffs2, i32 numBands);
+void SHScale(f32* pOutCoeffs, const f32* pCoeffs, f32 scale, i32 numBands);
+f32 SHDot(const f32* pCoeffs1, const f32* pCoeffs2, i32 numBands);
+void ZHProjectClampedCosineFunc(f32* pOutCoeffs, i32 numBands);
--- Source/Math/SphericalHarmonics.cpp
+++ Source/Math/SphericalHarmonics.cpp
@@ -32,55 +32,86 @@ i32 SHGetNumBasisFuncs(i32 numBands)
 	return numBands * numBands;
 }
 
-void SHEvaluateBasis(f32* pBasisFuncValues, i32 numBands, const Vector3f& dir)
+void SHEvaluateBasis(f32* pOutBasisFuncValues, i32 numBands, const Vector3f& dir)
 {
 	// Based on Appendix A2 Polynomial Forms of SH Basis
 	// from Stupid Spherical Harmonics Tricks by Peter-Pike Sloan 
 
-	assert(IsNormalized(dir));
+	assert(pOutBasisFuncValues != nullptr);
 	assert(IsInRange(MIN_NUM_BANDS, MAX_NUM_BANDS, numBands));
-	
-	pBasisFuncValues[0] = 0.282095f;
+	assert(IsNormalized(dir));
+		
+	pOutBasisFuncValues[0] = 0.282095f;
 	if (numBands == 1)
 		return;
 
-	pBasisFuncValues[1] = -0.488603f * dir.m_Y;
-	pBasisFuncValues[2] =  0.488603f * dir.m_Z;
-	pBasisFuncValues[3] = -0.488603f * dir.m_X;
+	pOutBasisFuncValues[1] = -0.488603f * dir.m_Y;
+	pOutBasisFuncValues[2] =  0.488603f * dir.m_Z;
+	pOutBasisFuncValues[3] = -0.488603f * dir.m_X;
 	if (numBands == 2)
 		return;
 
 	const Vector3f sqrDir = Sqr(dir);
 
-	pBasisFuncValues[4] =  1.092548f * dir.m_X * dir.m_Y;
-	pBasisFuncValues[5] = -1.092548f * dir.m_Y * dir.m_Z;
-	pBasisFuncValues[6] =  0.315392f * (3.0f * sqrDir.m_Z - 1.0f);
-	pBasisFuncValues[7] = -1.092548f * dir.m_X * dir.m_Z;
-	pBasisFuncValues[8] =  0.546274f * (sqrDir.m_X - sqrDir.m_Y);
+	pOutBasisFuncValues[4] =  1.092548f * dir.m_X * dir.m_Y;
+	pOutBasisFuncValues[5] = -1.092548f * dir.m_Y * dir.m_Z;
+	pOutBasisFuncValues[6] =  0.315392f * (3.0f * sqrDir.m_Z - 1.0f);
+	pOutBasisFuncValues[7] = -1.092548f * dir.m_X * dir.m_Z;
+	pOutBasisFuncValues[8] =  0.546274f * (sqrDir.m_X - sqrDir.m_Y);
 	if (numBands == 3)
 		return;
 
-	pBasisFuncValues[ 9] = -0.590044f * dir.m_Y * (3.0f * sqrDir.m_X - sqrDir.m_Y);
-	pBasisFuncValues[10] =  2.890611f * dir.m_X * dir.m_Y * dir.m_Z;
-	pBasisFuncValues[11] = -0.457046f * dir.m_Y * (5.0f * sqrDir.m_Z - 1.0f);
-	pBasisFuncValues[12] =  0.373176f * dir.m_Z * (5.0f * sqrDir.m_Z - 3.0f);
-	pBasisFuncValues[13] = -0.457046f * dir.m_X * (5.0f * sqrDir.m_Z - 1.0f);
-	pBasisFuncValues[14] =  1.445306f * dir.m_Z * (sqrDir.m_X - sqrDir.m_Y);
-	pBasisFuncValues[15] = -0.590044f * dir.m_X * (sqrDir.m_X - 3.0f * sqrDir.m_Y);
+	pOutBasisFuncValues[ 9] = -0.590044f * dir.m_Y * (3.0f * sqrDir.m_X - sqrDir.m_Y);
+	pOutBasisFuncValues[10] =  2.890611f * dir.m_X * dir.m_Y * dir.m_Z;
+	pOutBasisFuncValues[11] = -0.457046f * dir.m_Y * (5.0f * sqrDir.m_Z - 1.0f);
+	pOutBasisFuncValues[12] =  0.373176f * dir.m_Z * (5.0f * sqrDir.m_Z - 3.0f);
+	pOutBasisFuncValues[13] = -0.457046f * dir.m_X * (5.0f * sqrDir.m_Z - 1.0f);
+	pOutBasisFuncValues[14] =  1.445306f * dir.m_Z * (sqrDir.m_X - sqrDir.m_Y);
+	pOutBasisFuncValues[15] = -0.590044f * dir.m_X * (sqrDir.m_X - 3.0f * sqrDir.m_Y);
 	if (numBands == 4)
 		return;
 	
-	pBasisFuncValues[16] =  2.503343f * dir.m_X * dir.m_Y * (sqrDir.m_X - sqrDir.m_Y);
-	pBasisFuncValues[17] = -1.770131f * dir.m_Y * dir.m_Z * (3.0f * sqrDir.m_X - sqrDir.m_Y);
-	pBasisFuncValues[18] =  0.946175f * dir.m_X * dir.m_Y * (7.0f * sqrDir.m_Z - 1.0f);
-	pBasisFuncValues[19] = -0.669047f * dir.m_Y * dir.m_Z * (7.0f * sqrDir.m_Z - 3.0f);
-	pBasisFuncValues[20] =  0.105786f * (35.0f * Sqr(sqrDir.m_Z) - 30.0f * sqrDir.m_Z + 3.0f);
-	pBasisFuncValues[21] = -0.669047f * dir.m_X * dir.m_Z * (7.0f * sqrDir.m_Z - 3.0f);
-	pBasisFuncValues[22] =  0.473087f * (sqrDir.m_X - sqrDir.m_Y) * (7.0f * sqrDir.m_Z - 1.0f);
-	pBasisFuncValues[23] = -1.770131f * dir.m_X * dir.m_Z * (sqrDir.m_X - 3.0f * sqrDir.m_Y);
-	pBasisFuncValues[24] =  0.625836f * (Sqr(sqrDir.m_X) - 6.0f * sqrDir.m_X * sqrDir.m_Y + Sqr(sqrDir.m_Y));
+	pOutBasisFuncValues[16] =  2.503343f * dir.m_X * dir.m_Y * (sqrDir.m_X - sqrDir.m_Y);
+	pOutBasisFuncValues[17] = -1.770131f * dir.m_Y * dir.m_Z * (3.0f * sqrDir.m_X - sqrDir.m_Y);
+	pOutBasisFuncValues[18] =  0.946175f * dir.m_X * dir.m_Y * (7.0f * sqrDir.m_Z - 1.0f);
+	pOutBasisFuncValues[19] = -0.669047f * dir.m_Y * dir.m_Z * (7.0f * sqrDir.m_Z - 3.0f);
+	pOutBasisFuncValues[20] =  0.105786f * (35.0f * Sqr(sqrDir.m_Z) - 30.0f * sqrDir.m_Z + 3.0f);
+	pOutBasisFuncValues[21] = -0.669047f * dir.m_X * dir.m_Z * (7.0f * sqrDir.m_Z - 3.0f);
+	pOutBasisFuncValues[22] =  0.473087f * (sqrDir.m_X - sqrDir.m_Y) * (7.0f * sqrDir.m_Z - 1.0f);
+	pOutBasisFuncValues[23] = -1.770131f * dir.m_X * dir.m_Z * (sqrDir.m_X - 3.0f * sqrDir.m_Y);
+	pOutBasisFuncValues[24] =  0.625836f * (Sqr(sqrDir.m_X) - 6.0f * sqrDir.m_X * sqrDir.m_Y + Sqr(sqrDir.m_Y));
 	if (numBands == 5)
 		return;
+}
+
+void SHAdd(f32* pOutCoeffs, const f32* pCoeffs1, const f32* pCoeffs2, i32 numBands)
+{
+	assert((pOutCoeffs != nullptr) && (pCoeffs1 != nullptr) && (pCoeffs2 != nullptr));
+	
+	const i32 numBasisFuncs = SHGetNumBasisFuncs(numBands);
+	for (i32 i = 0; i < numBasisFuncs; ++i)
+		pOutCoeffs[i] = pCoeffs1[i] + pCoeffs2[i];
+}
+
+void SHScale(f32* pOutCoeffs, const f32* pCoeffs, f32 scale, i32 numBands)
+{
+	assert((pOutCoeffs != nullptr) && (pCoeffs != nullptr));
+	
+	const i32 numBasisFuncs = SHGetNumBasisFuncs(numBands);
+	for (i32 i = 0; i < numBasisFuncs; ++i)
+		pOutCoeffs[i] = scale * pCoeffs[i];
+}
+
+f32 SHDot(const f32* pCoeffs1, const f32* pCoeffs2, i32 numBands)
+{
+	assert((pCoeffs1 != nullptr) && (pCoeffs2 != nullptr));
+
+	f32 dot = 0.0f;
+	const i32 numBasisFuncs = SHGetNumBasisFuncs(numBands);		
+	for (i32 i = 0; i < numBasisFuncs; ++i)
+		dot += pCoeffs1[i] * pCoeffs2[i];
+	
+	return dot;
 }
 
 i32 SHGetBasisFuncIndex(i32 l, i32 m)
@@ -88,30 +119,31 @@ i32 SHGetBasisFuncIndex(i32 l, i32 m)
 	return (l * (l + 1) + m);
 }
 
-void ZHProjectClampedCosineFunc(f32* pCoeffs, i32 numBands)
+void ZHProjectClampedCosineFunc(f32* pOutCoeffs, i32 numBands)
 {
 	// Based on Properties of the Convolution Kernel
 	// from Lambertian Reflectance and Linear Subspaces by Ronen Basri and Dawid W. Jacobs
-		
+	
+	assert(pOutCoeffs != nullptr);
 	assert(IsInRange(MIN_NUM_BANDS, MAX_NUM_BANDS, numBands));
 	
-	pCoeffs[0] = 0.886227f;
+	pOutCoeffs[0] = 0.886227f;
 	if (numBands == 1)
 		return;
 
-	pCoeffs[1] = 1.023327f;
+	pOutCoeffs[1] = 1.023327f;
 	if (numBands == 2)
 		return;
 
-	pCoeffs[2] = 0.495416f;
+	pOutCoeffs[2] = 0.495416f;
 	if (numBands == 3)
 		return;
 
-	pCoeffs[3] = 0.0f;
+	pOutCoeffs[3] = 0.0f;
 	if (numBands == 4)
 		return;
 
-	pCoeffs[4] = -0.110778f;
+	pOutCoeffs[4] = -0.110778f;
 	if (numBands == 5)
 		return;
 }
