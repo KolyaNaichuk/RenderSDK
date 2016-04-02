16226fcbc85082fa68a8b8fec463f662899af0a4
Kolya Naichuk <thyvanitas@gmail.com>
2016-04-02 10:09:48 +0200

    Added functionality for point, spot and directional lights

--- Shaders/Lighting.hlsl
+++ Shaders/Lighting.hlsl
@@ -1,8 +1,8 @@
 #ifndef __LIGHTING__
 #define __LIGHTING__
 
-float3 CalcPhongLighting(float3 normal, float3 dirToViewer, float3 dirToLight,
-	float3 lightColor, float3 diffuseAlbedo, float3 specularAlbedo, float specularPower)
+float3 CalcPhongLighting(float3 dirToViewer, float3 dirToLight, float3 lightColor,
+	float3 normal, float3 diffuseAlbedo, float3 specularAlbedo, float specularPower)
 {
 	float NdotL = saturate(dot(normal, dirToLight));
 	float3 diffuseColor = NdotL * diffuseAlbedo * lightColor;
@@ -14,8 +14,8 @@ float3 CalcPhongLighting(float3 normal, float3 dirToViewer, float3 dirToLight,
 	return (diffuseColor + specularColor);
 }
 
-float3 CalcBlinnPhongLighting(float3 normal, float3 dirToViewer, float3 dirToLight,
-	float3 lightColor, float3 diffuseAlbedo, float3 specularAlbedo, float specularPower)
+float3 CalcBlinnPhongLighting(float3 dirToViewer, float3 dirToLight, float3 lightColor,
+	float3 normal, float3 diffuseAlbedo, float3 specularAlbedo, float specularPower)
 {
 	float NdotL = saturate(dot(normal, dirToLight));
 	float3 diffuseColor = NdotL * diffuseAlbedo * lightColor;
@@ -27,6 +27,18 @@ float3 CalcBlinnPhongLighting(float3 normal, float3 dirToViewer, float3 dirToLig
 	return (diffuseColor + specularColor);
 }
 
+float3 CalcLighting(float3 dirToViewer, float3 dirToLight, float3 lightColor,
+	float3 normal, float3 diffuseAlbedo, float3 specularAlbedo, float specularPower)
+{
+#ifdef PHONG_LIGHTING
+	return CalcPhongLighting(dirToViewer, dirToLight, lightColor, normal, diffuseAlbedo, specularAlbedo, specularPower);
+#endif // PHONG_LIGHTING
+
+#ifdef BLINN_PHONG_LIGHTING
+	return CalcBlinnPhongLighting(dirToViewer, dirToLight, lightColor, normal, diffuseAlbedo, specularAlbedo, specularPower);
+#endif // BLINN_PHONG_LIGHTING
+}
+
 float CalcLightDistanceAttenuation(float distToLight, float attenStartRange, float attenEndRange)
 {
 	return saturate((attenEndRange - distToLight) / (attenEndRange - attenStartRange));
@@ -35,6 +47,45 @@ float CalcLightDistanceAttenuation(float distToLight, float attenStartRange, flo
 float CalcLightDirectionAttenuation(float cosDirAngle, float cosHalfInnerConeAngle, float cosHalfOuterConeAngle)
 {
 	return smoothstep(cosHalfOuterConeAngle, cosHalfInnerConeAngle, cosDirAngle);
+}
+
+float3 CalcPointLightContribution(float3 lightPos, float3 lightColor, float attenStartRange, float attenEndRange,
+	float3 dirToViewer, float3 position, float3 normal, float3 diffuseAlbedo, float3 specularAlbedo, float specularPower)
+{
+	float3 dirToLight = lightPos - position;
+	float distToLight = length(dirToLight);
+	dirToLight *= rcp(distToLight);
+
+	float3 lightContrib = CalcLighting(dirToViewer, dirToLight, lightColor, normal, diffuseAlbedo, specularAlbedo, specularPower);
+	float distAtten = CalcLightDistanceAttenuation(distToLight, attenStartRange, attenEndRange);
+
+	return distAtten * lightContrib;
+}
+
+float3 CalcSpotLightContribution(float3 lightPos, float3 lightDir, float3 lightColor, float attenStartRange, float attenEndRange,
+	float cosHalfInnerConeAngle, float cosHalfOuterConeAngle, float3 dirToViewer, float3 position, float3 normal,
+	float3 diffuseAlbedo, float3 specularAlbedo, float specularPower)
+{
+	float3 dirToLight = lightPos - position;
+	float distToLight = length(dirToLight);
+	dirToLight *= rcp(distToLight);
+
+	float3 lightContrib = CalcLighting(dirToViewer, dirToLight, lightColor, normal, diffuseAlbedo, specularAlbedo, specularPower);
+	float distAtten = CalcLightDistanceAttenuation(distToLight, attenStartRange, attenEndRange);
+	
+	float cosDirAngle = dot(lightDir, -dirToLight);
+	float dirAtten = CalcLightDirectionAttenuation(cosDirAngle, cosHalfInnerConeAngle, cosHalfOuterConeAngle);
+
+	return (distAtten * dirAtten) * lightContrib;
+}
+
+float3 CalcDirectionalLightContribution(float3 lightDir, float3 lightColor, float3 dirToViewer, float3 normal,
+	float3 diffuseAlbedo, float3 specularAlbedo, float specularPower)
+{
+	float3 dirToLight = -lightDir;
+	float3 lightContrib = CalcLighting(dirToViewer, dirToLight, lightColor, normal, diffuseAlbedo, specularAlbedo, specularPower);
+
+	return lightContrib;
 }
 
 #endif // __LIGHTING__
