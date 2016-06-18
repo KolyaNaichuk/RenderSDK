806add52d02ece58d2c28ddafce32a1778fd08cf
Kolya Naichuk <thyvanitas@gmail.com>
2016-06-18 20:41:21 +0200

    Added InfoQueue

--- Source/DX/DXDevice.cpp
+++ Source/DX/DXDevice.cpp
@@ -28,6 +28,16 @@ void GetHardwareAdapter(DXFactory* pFactory, D3D_FEATURE_LEVEL minFeatureLevel, 
 
 DXDevice::DXDevice(DXFactory* pFactory, D3D_FEATURE_LEVEL minFeatureLevel, bool useWarpAdapter)
 {
+#ifdef _DEBUG
+	ComPtr<ID3D12Debug> d3dDebug;
+	DXVerify(D3D12GetDebugInterface(IID_PPV_ARGS(&d3dDebug)));
+	d3dDebug->EnableDebugLayer();
+
+	ComPtr<IDXGIDebug1> dxgiDebug;
+	DXVerify(DXGIGetDebugInterface1(0, IID_PPV_ARGS(&dxgiDebug)));
+	DXVerify(dxgiDebug->ReportLiveObjects(DXGI_DEBUG_ALL, DXGI_DEBUG_RLO_ALL));
+#endif
+
 	if (useWarpAdapter)
 	{
 		ComPtr<IDXGIAdapter> warpAdapter;
@@ -42,6 +52,13 @@ DXDevice::DXDevice(DXFactory* pFactory, D3D_FEATURE_LEVEL minFeatureLevel, bool 
 
 		DXVerify(D3D12CreateDevice(hardwareAdapter.Get(), minFeatureLevel, IID_PPV_ARGS(GetDXObjectAddress())));
 	}
+
+#ifdef _DEBUG
+	ComPtr<ID3D12InfoQueue> d3dInfoQueue;
+	DXVerify(GetDXObject()->QueryInterface(IID_PPV_ARGS(&d3dInfoQueue)));
+	DXVerify(d3dInfoQueue->SetBreakOnSeverity(D3D12_MESSAGE_SEVERITY_WARNING, TRUE));
+	DXVerify(d3dInfoQueue->SetBreakOnSeverity(D3D12_MESSAGE_SEVERITY_ERROR, TRUE));
+#endif
 }
 
 void DXDevice::CheckFeatureSupport(D3D12_FEATURE feature, void* pFeatureSupportData, UINT featureSupportDataSize)
--- Source/DX/DXFactory.cpp
+++ Source/DX/DXFactory.cpp
@@ -1,12 +1,7 @@
 #include "DX/DXFactory.h"
-#include "DX/DXDebug.h"
 
 DXFactory::DXFactory()
 {
-#ifdef _DEBUG
-	DXDebug debugLayer;
-	debugLayer.EnableDebugLayer();
-#endif
 	DXVerify(CreateDXGIFactory1(IID_PPV_ARGS(GetDXObjectAddress())));
 }
 
