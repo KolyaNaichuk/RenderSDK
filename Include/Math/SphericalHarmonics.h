318c55796d25796a9c805b8e2d036b40e3c88685
Kolya Naichuk <thyvanitas@gmail.com>
2016-11-06 19:47:36 +0100

    More functionality for beloved spherical harmonics

--- Include/Math/SphericalHarmonics.h
+++ Include/Math/SphericalHarmonics.h
@@ -5,12 +5,30 @@
 class Radian;
 struct Vector3f;
 
+enum SHProjectinFlags
+{
+	SHProjectionFlags_DistanceAttenuation = 1 << 0,
+	SHProjectionFlags_Normalization = 1 << 1
+};
+
+
 f32 SHEvaluate(i32 l, i32 m, const Radian& theta, const Radian& phi);
 
-i32 SHGetBasisFuncIndex(i32 l, i32 m);
-i32 SHGetNumBasisFuncs(i32 numBands);
+constexpr i32 SHGetBasisFuncIndex(i32 l, i32 m) { return (l * (l + 1) + m); }
+constexpr i32 SHGetNumBasisFuncs(i32 numBands) { return numBands * numBands; }
+
 void SHEvaluateBasis(f32* pOutBasisFuncValues, i32 numBands, const Vector3f& dir);
 void SHAdd(f32* pOutCoeffs, const f32* pCoeffs1, const f32* pCoeffs2, i32 numBands);
 void SHScale(f32* pOutCoeffs, const f32* pCoeffs, f32 scale, i32 numBands);
-f32 SHDot(const f32* pCoeffs1, const f32* pCoeffs2, i32 numBands);
-void ZHProjectClampedCosineFunc(f32* pOutCoeffs, i32 numBands);
+f32 SHProduct(const f32* pCoeffs1, const f32* pCoeffs2, i32 numBands);
+
+void SHProjectPointLight(f32* pOutRCoeffs, f32* pOutGCoeffs, f32* pOutBCoeffs,
+	i32 numBands, const Vector3f& surfacePoint, const Vector3f& lightPos, Vector3f lightRadiance, u8 flags);
+
+void SHProjectDirectionalLight(f32* pOutRCoeffs, f32* pOutGCoeffs, f32* pOutBCoeffs,
+	i32 numBands, const Vector3f& dir, Vector3f lightRadiance, u8 flags);
+
+void SHProjectCubeMap();
+
+void ZHProjectCosineLobeOrientedAlongZAxis(f32* pOutCoeffs, i32 numBands);
+void SHProjectCosineLobe(f32* pOutCoeffs, i32 numBands, const Vector3f& dir);
--- Source/Math/SphericalHarmonics.cpp
+++ Source/Math/SphericalHarmonics.cpp
@@ -6,7 +6,7 @@ namespace
 {
 	const int MIN_NUM_BANDS = 1;
 	const int MAX_NUM_BANDS = 5;
-
+		
 	f32 K(i32 l, i32 m);
 	i32 Factorial(i32 number);
 	i32 DoubleFactorial(i32 number);
@@ -27,11 +27,6 @@ f32 SHEvaluate(i32 l, i32 m, const Radian& theta, const Radian& phi)
 	return K(l, m) * Legendre(l, m, Cos(theta));
 }
 
-i32 SHGetNumBasisFuncs(i32 numBands)
-{
-	return numBands * numBands;
-}
-
 void SHEvaluateBasis(f32* pOutBasisFuncValues, i32 numBands, const Vector3f& dir)
 {
 	// Based on Appendix A2 Polynomial Forms of SH Basis
@@ -40,7 +35,7 @@ void SHEvaluateBasis(f32* pOutBasisFuncValues, i32 numBands, const Vector3f& dir
 	assert(pOutBasisFuncValues != nullptr);
 	assert(IsInRange(MIN_NUM_BANDS, MAX_NUM_BANDS, numBands));
 	assert(IsNormalized(dir));
-		
+	
 	pOutBasisFuncValues[0] = 0.282095f;
 	if (numBands == 1)
 		return;
@@ -87,7 +82,7 @@ void SHEvaluateBasis(f32* pOutBasisFuncValues, i32 numBands, const Vector3f& dir
 void SHAdd(f32* pOutCoeffs, const f32* pCoeffs1, const f32* pCoeffs2, i32 numBands)
 {
 	assert((pOutCoeffs != nullptr) && (pCoeffs1 != nullptr) && (pCoeffs2 != nullptr));
-	
+		
 	const i32 numBasisFuncs = SHGetNumBasisFuncs(numBands);
 	for (i32 i = 0; i < numBasisFuncs; ++i)
 		pOutCoeffs[i] = pCoeffs1[i] + pCoeffs2[i];
@@ -102,10 +97,10 @@ void SHScale(f32* pOutCoeffs, const f32* pCoeffs, f32 scale, i32 numBands)
 		pOutCoeffs[i] = scale * pCoeffs[i];
 }
 
-f32 SHDot(const f32* pCoeffs1, const f32* pCoeffs2, i32 numBands)
+f32 SHProduct(const f32* pCoeffs1, const f32* pCoeffs2, i32 numBands)
 {
 	assert((pCoeffs1 != nullptr) && (pCoeffs2 != nullptr));
-
+	
 	f32 dot = 0.0f;
 	const i32 numBasisFuncs = SHGetNumBasisFuncs(numBands);		
 	for (i32 i = 0; i < numBasisFuncs; ++i)
@@ -114,12 +109,66 @@ f32 SHDot(const f32* pCoeffs1, const f32* pCoeffs2, i32 numBands)
 	return dot;
 }
 
-i32 SHGetBasisFuncIndex(i32 l, i32 m)
+void SHProjectPointLight(f32* pOutRCoeffs, f32* pOutGCoeffs, f32* pOutBCoeffs, i32 numBands, const Vector3f& surfacePoint, const Vector3f& lightPos, Vector3f lightRadiance, u8 flags)
 {
-	return (l * (l + 1) + m);
+	// Based on Physically Based Rendering (From Theory to Implementation), 2nd edition 
+	// p. 942 Point Lights
+			
+	Vector3f dirOnHemisphere = lightPos - surfacePoint;
+	f32 basisFuncValues[SHGetNumBasisFuncs(MAX_NUM_BANDS)];
+	SHEvaluateBasis(basisFuncValues, numBands, Normalize(dirOnHemisphere));
+
+	if ((flags & SHProjectionFlags_DistanceAttenuation) != 0)
+		lightRadiance /= LengthSquared(dirOnHemisphere);
+	
+	if ((flags & SHProjectionFlags_Normalization) != 0)
+		assert(false);
+	
+	const i32 numBasisFuncs = SHGetNumBasisFuncs(numBands);
+
+	assert(pOutRCoeffs != nullptr);
+	for (i32 i = 0; i < numBasisFuncs; ++i)
+		pOutRCoeffs[i] = basisFuncValues[i] * lightRadiance.m_X;
+	
+	if (pOutGCoeffs != nullptr)
+	{
+		for (i32 i = 0; i < numBasisFuncs; ++i)
+			pOutGCoeffs[i] = basisFuncValues[i] * lightRadiance.m_Y;
+	}
+	if (pOutBCoeffs != nullptr)
+	{
+		for (i32 i = 0; i < numBasisFuncs; ++i)
+			pOutBCoeffs[i] = basisFuncValues[i] * lightRadiance.m_Z;
+	}
 }
 
-void ZHProjectClampedCosineFunc(f32* pOutCoeffs, i32 numBands)
+void SHProjectDirectionalLight(f32* pOutRCoeffs, f32* pOutGCoeffs, f32* pOutBCoeffs, i32 numBands, const Vector3f& dir, Vector3f lightRadiance, u8 flags)
+{		
+	f32 basisFuncValues[SHGetNumBasisFuncs(MAX_NUM_BANDS)];
+	SHEvaluateBasis(basisFuncValues, numBands, dir);
+
+	if ((flags & SHProjectionFlags_Normalization) != 0)
+		assert(false);
+	
+	const i32 numBasisFuncs = SHGetNumBasisFuncs(numBands);
+
+	assert(pOutRCoeffs != nullptr);
+	for (i32 i = 0; i < numBasisFuncs; ++i)
+		pOutRCoeffs[i] = basisFuncValues[i] * lightRadiance.m_X;
+
+	if (pOutGCoeffs != nullptr)
+	{
+		for (i32 i = 0; i < numBasisFuncs; ++i)
+			pOutGCoeffs[i] = basisFuncValues[i] * lightRadiance.m_Y;
+	}
+	if (pOutBCoeffs != nullptr)
+	{
+		for (i32 i = 0; i < numBasisFuncs; ++i)
+			pOutBCoeffs[i] = basisFuncValues[i] * lightRadiance.m_Z;
+	}
+}
+
+void ZHProjectCosineLobeOrientedAlongZAxis(f32* pOutCoeffs, i32 numBands)
 {
 	// Based on Properties of the Convolution Kernel
 	// from Lambertian Reflectance and Linear Subspaces by Ronen Basri and Dawid W. Jacobs
@@ -144,6 +193,68 @@ void ZHProjectClampedCosineFunc(f32* pOutCoeffs, i32 numBands)
 		return;
 
 	pOutCoeffs[4] = -0.110778f;
+	if (numBands == 5)
+		return;
+}
+
+void SHProjectCosineLobe(f32* pOutCoeffs, i32 numBands, const Vector3f& dir)
+{
+	// Based on Zonal Harmonics section from Stupid Spherical Harmonics Tricks by Peter-Pike Sloan
+	// and Efficient Representation for Irradiance Environment Maps by Ramamoorthi and Hanrahan.
+	// We compute zonal harmonics coefficients for cosine lobe oriented along Z axis.
+	// Then, we rotate them so that they are aligned with actual direction of the cosine lobe.
+	
+	assert(pOutCoeffs != nullptr);
+	assert(IsInRange(MIN_NUM_BANDS, MAX_NUM_BANDS, numBands));
+
+	f32 basisFuncValues[SHGetNumBasisFuncs(MAX_NUM_BANDS)];
+	SHEvaluateBasis(basisFuncValues, numBands, dir);
+
+	f32 zonalCosineLobeCoeffs[MAX_NUM_BANDS];
+	ZHProjectCosineLobeOrientedAlongZAxis(zonalCosineLobeCoeffs, numBands);
+	
+	zonalCosineLobeCoeffs[0] *= Sqrt(4.0f * PI); // A0 = 3.141593f
+	pOutCoeffs[0] = zonalCosineLobeCoeffs[0] * basisFuncValues[0];
+	if (numBands == 1)
+		return;
+
+	zonalCosineLobeCoeffs[1] *= Sqrt(4.0f * PI / 3.0f); // A1 = 2.094395f
+	pOutCoeffs[1] = zonalCosineLobeCoeffs[1] * basisFuncValues[1];
+	pOutCoeffs[2] = zonalCosineLobeCoeffs[1] * basisFuncValues[2];
+	pOutCoeffs[3] = zonalCosineLobeCoeffs[1] * basisFuncValues[3];
+	if (numBands == 2)
+		return;
+
+	zonalCosineLobeCoeffs[2] *= Sqrt(4.0f * PI / 5.0f); // A2 = 0.785398f
+	pOutCoeffs[4] = zonalCosineLobeCoeffs[2] * basisFuncValues[4];
+	pOutCoeffs[5] = zonalCosineLobeCoeffs[2] * basisFuncValues[5];
+	pOutCoeffs[6] = zonalCosineLobeCoeffs[2] * basisFuncValues[6];
+	pOutCoeffs[7] = zonalCosineLobeCoeffs[2] * basisFuncValues[7];
+	pOutCoeffs[8] = zonalCosineLobeCoeffs[2] * basisFuncValues[8];
+	if (numBands == 3)
+		return;
+
+	// zonalCosineLobeCoeffs[3] = 0.0f, A3 = 0.0f
+	pOutCoeffs[ 9] = 0.0f;
+	pOutCoeffs[10] = 0.0f;
+	pOutCoeffs[11] = 0.0f;
+	pOutCoeffs[12] = 0.0f;
+	pOutCoeffs[13] = 0.0f;
+	pOutCoeffs[14] = 0.0f;
+	pOutCoeffs[15] = 0.0f;
+	if (numBands == 4)
+		return;
+	
+	zonalCosineLobeCoeffs[4] *= Sqrt(4.0f * PI / 9.0f); // A4 = -0.130900
+	pOutCoeffs[16] = zonalCosineLobeCoeffs[4] * basisFuncValues[16];
+	pOutCoeffs[17] = zonalCosineLobeCoeffs[4] * basisFuncValues[17];
+	pOutCoeffs[18] = zonalCosineLobeCoeffs[4] * basisFuncValues[18];
+	pOutCoeffs[19] = zonalCosineLobeCoeffs[4] * basisFuncValues[19];
+	pOutCoeffs[20] = zonalCosineLobeCoeffs[4] * basisFuncValues[20];
+	pOutCoeffs[21] = zonalCosineLobeCoeffs[4] * basisFuncValues[21];
+	pOutCoeffs[22] = zonalCosineLobeCoeffs[4] * basisFuncValues[22];
+	pOutCoeffs[23] = zonalCosineLobeCoeffs[4] * basisFuncValues[23];
+	pOutCoeffs[24] = zonalCosineLobeCoeffs[4] * basisFuncValues[24];
 	if (numBands == 5)
 		return;
 }
