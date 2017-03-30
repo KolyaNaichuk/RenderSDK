98260354585c9906d8c53efd1abe7b4eab57ee62
Kolya Naichuk <thyvanitas@gmail.com>
2017-03-30 19:19:20 +0200

    Using C++ 11 built in types

--- Include/Common/BasicTypes.h
+++ Include/Common/BasicTypes.h
@@ -1,14 +1,16 @@
 #pragma once
 
-typedef signed __int64		i64;
-typedef signed int			i32;
-typedef signed short		i16;
-typedef signed char			i8;
+#include <cstdint>
 
-typedef unsigned __int64	u64;
-typedef unsigned int		u32;
-typedef unsigned short		u16;
-typedef unsigned char		u8;
+typedef std::int64_t		i64;
+typedef std::int32_t		i32;
+typedef std::int16_t		i16;
+typedef std::int8_t			i8;
+
+typedef std::uint64_t		u64;
+typedef std::uint32_t		u32;
+typedef std::uint16_t		u16;
+typedef std::uint8_t		u8;
 
 typedef float				f32;
 typedef double				f64;
