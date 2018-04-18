8ced1c1012824493f8548aa0a7287de95cd281b0
Kolya Naichuk <thyvanitas@gmail.com>
2018-04-18 13:13:11 +0200

    Optimizing SAT 2

--- Shaders/CreateTiledShadowMapSATCS.hlsl
+++ Shaders/CreateTiledShadowMapSATCS.hlsl
@@ -8,16 +8,19 @@ groupshared float2 g_SharedData[TILE_SIZE][TILE_SIZE];
 void Main(uint3 groupId : SV_GroupID, uint threadIndex : SV_GroupIndex)
 {
 	const uint2 globalOffset = g_TileOffsetBuffer[groupId.x];
-
-	g_SharedData[0][threadIndex] = g_TiledVarianceShadowMap[globalOffset + uint2(0, threadIndex)];
-	[unroll] for (uint x = 1; x < TILE_SIZE; ++x)
-		g_SharedData[x][threadIndex] = g_SharedData[x - 1][threadIndex] + g_TiledVarianceShadowMap[globalOffset + uint2(x, threadIndex)];
-	GroupMemoryBarrierWithGroupSync();
-
-	[unroll] for (uint y = 1; y < TILE_SIZE; ++y)
-		g_SharedData[threadIndex][y] += g_SharedData[threadIndex][y - 1];
-	GroupMemoryBarrierWithGroupSync();
-
+	
+	float2 rowPrefixSum = float2(0.0f, 0.0f);
 	[unroll] for (uint x = 0; x < TILE_SIZE; ++x)
-		g_TiledVarianceShadowMapSAT[globalOffset + uint2(x, threadIndex)] = g_SharedData[x][threadIndex];
+	{
+		rowPrefixSum += g_TiledVarianceShadowMap[globalOffset + uint2(x, threadIndex)];
+		g_SharedData[x][threadIndex] = rowPrefixSum;
+	}
+	GroupMemoryBarrierWithGroupSync();
+
+	float2 columnPrefixSum = float2(0.0f, 0.0f);
+	[unroll] for (uint y = 0; y < TILE_SIZE; ++y)
+	{
+		columnPrefixSum += g_SharedData[threadIndex][y];
+		g_TiledVarianceShadowMapSAT[globalOffset + uint2(threadIndex, y)] = columnPrefixSum;
+	}
 }
