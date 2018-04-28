9c9facf921d554f81e67854a3ec53be6e737a06c
Kolya Naichuk <thyvanitas@gmail.com>
2018-04-28 14:12:03 +0200

    To previous commit. Faking exp for now

--- Shaders/CreateTiledExpShadowMapPS.hlsl
+++ Shaders/CreateTiledExpShadowMapPS.hlsl
@@ -6,7 +6,7 @@ struct PSInput
 	uint tileId				: SV_InstanceID;
 };
 
-struct ConvertShadowMapParams
+struct CreateExpShadowMapParams
 {
 	float lightViewNearPlane;
 	float lightRcpViewClipRange;
@@ -15,9 +15,9 @@ struct ConvertShadowMapParams
 };
 
 Texture2D<float> g_TiledShadowMap : register(t0);
-StructuredBuffer<ConvertShadowMapParams> g_ConvertShadowMapParamsBuffer : register(t1);
+StructuredBuffer<CreateExpShadowMapParams> g_CreateExpShadowMapParamsBuffer : register(t1);
 
-float3 Main(PSInput input) : SV_Target
+float2 Main(PSInput input) : SV_Target
 {
 #if LIGHT_TYPE == LIGHT_TYPE_POINT
 	uint lightIndex = input.tileId / NUM_CUBE_MAP_FACES;
@@ -27,12 +27,12 @@ float3 Main(PSInput input) : SV_Target
 	uint lightIndex = input.tileId;
 #endif // LIGHT_TYPE_SPOT
 
-	ConvertShadowMapParams params = g_ConvertShadowMapParamsBuffer[lightIndex];
+	CreateExpShadowMapParams params = g_CreateExpShadowMapParamsBuffer[lightIndex];
 	uint2 pixelPos = uint2(input.screenSpacePos.xy);
 
 	float hardwareDepth = g_TiledShadowMap[pixelPos].r;
 	float lightSpaceDepth = params.lightProjMatrix43 / (hardwareDepth - params.lightProjMatrix33);
 	float normalizedLightSpaceDepth = (lightSpaceDepth - params.lightViewNearPlane) * params.lightRcpViewClipRange;
 
-	return float3(normalizedLightSpaceDepth, 0.5f, 1.0f);
+	return float2(normalizedLightSpaceDepth, normalizedLightSpaceDepth);
 }
